c1dfdd349f7084d027a9cd05d1270646c8ada848
Tom Ricciuti <tom.ricciuti@web.de>
2024-11-10 12:59:33 +0100

    First working version of B1A1

--- B1A1.c
+++ B1A1.c
@@ -4,24 +4,32 @@
 #include <stdlib.h>
 
 bool check_if_palindrom(char* word_to_check){
-    if(word_to_check != NULL){
-        return true;
-    }else{
-        return false;
+    int word_length = strlen(word_to_check);
+
+    for(int i = 0; i<=(word_length/2); i++){
+        //Erweiterbar mit Großbuchstabencheck
+        if(word_to_check[i] != word_to_check[word_length-(i+1)]){
+            return false;
+        }
     }
+    return true;
 }
 
 int number_of_palindroms(char* argv[]){
     int palindrom_counter = 0; 
+    int Iintervall_start = 0;
+    int Iintervall_end = 0;
 
     char * intervall_start = strtok(argv[2],":");
     char * intervall_end = strtok(NULL,":");
-    int Iintervall_start = (int)intervall_start;
-    int Iintervall_end = (int)intervall_end;
     if(intervall_end == NULL){
         printf("Please enter correct intervall as in 3:6");
         exit(1);
     }
+    sscanf(intervall_start,"%d", &Iintervall_start);
+    sscanf(intervall_end,"%d", &Iintervall_end);
+    printf("%d\n", Iintervall_start);
+    printf("%d\n", Iintervall_end);
 
     char * delimiters = ",";
     char * name = strtok(argv[1], delimiters);
